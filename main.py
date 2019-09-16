3becb8554908027f511e381b4d0f307e0e993e6b
MOODY Greg <gmoody@altranuk.local>
2019-09-16 10:26:13 +0100

    Added main script

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-def main():
-    print("Welcome to BusBoard.")
+import requests
+import json
+import sys
+
+stop_code='0180BAC30592'
+
+data=requests.get('https://api.github.com/events')
+print(data.text)
+
+sys.exit()
+
+#def main(stop_code):
+#    print("Welcome to BusBoard.")
 
 
-if __name__ == "__main__": main()
+
+#if __name__ == "__main__": main()
