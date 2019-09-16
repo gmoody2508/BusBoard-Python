a2a8742843ef79b187ed87b940c8c540ada21e10
MOODY Greg <gmoody@altranuk.local>
2019-09-16 11:02:05 +0100

    Can read webpage as json

--- main.py
+++ main.py
@@ -4,14 +4,37 @@ import sys
 
 stop_code='0180BAC30592'
 
-data=requests.get('https://api.github.com/events')
-print(data.text)
+#data=requests.get('https://api.github.com/events')
 
-sys.exit()
+KeyInfo_file=open("TransportAPIKey.txt",'r')
+KeyInfo=[]
+for line in KeyInfo_file:
+    line=line.split(':')
+    KeyInfo.append(line[1])
 
-#def main(stop_code):
-#    print("Welcome to BusBoard.")
+KeyInfo_file.close()
+AppID=KeyInfo[0]
+AppID=AppID[1:-1]
+Key=KeyInfo[1]
+Key=Key[1:]
+print(AppID)
+print(Key)
 
+def main(stop_code, AppID, Key):
+    print("Welcome to BusBoard.")
+    URL='https://transportapi.com/v3/uk/bus/stop/'
+    URL=URL+stop_code
+    URL=URL+"/live.json?app_id="
+    URL=URL+AppID
+    URL=URL+"&app_key="
+    URL=URL+Key
+    URL=URL+"&group=route&limit=5&nextbuses=yes"
+    print(URL)
+    bus_data=requests.get(URL)
+    print(bus_data)
+    bus_data=bus_data.json()
+    print(bus_data)
+    sys.exit()
 
-
+main(stop_code,AppID,Key)
 #if __name__ == "__main__": main()
